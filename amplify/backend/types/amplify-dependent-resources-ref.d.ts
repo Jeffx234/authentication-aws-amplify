4975f9e50a0f4f06637d81ba167365d43a3ab969
healthmobile <app@healthmobile.com.br>
2022-10-07 14:51:26 -0300

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "myappce15b934": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
-import '../styles/globals.css'
+import {Amplify} from "aws-amplify"
+import awsExports from "../src/aws-exports"
+
+Amplify.configure( awsExports )
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
